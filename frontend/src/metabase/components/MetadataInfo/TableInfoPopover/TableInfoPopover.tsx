ad5817de3eccd9a664bdf9710dbae29a4e01de8f
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-01-05 13:20:59 -0800

    Hide table info popover when the table has no description

--- frontend/src/metabase/components/MetadataInfo/TableInfoPopover/TableInfoPopover.tsx
+++ frontend/src/metabase/components/MetadataInfo/TableInfoPopover/TableInfoPopover.tsx
@@ -1,33 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import { hideAll } from "tippy.js";
 
 import TippyPopover, {
   ITippyPopoverProps,
 } from "metabase/components/Popover/TippyPopover";
+import Tables from "metabase/entities/tables";
+import Table from "metabase-lib/lib/metadata/Table";
 
 import { WidthBoundTableInfo } from "./TableInfoPopover.styled";
 
 export const POPOVER_DELAY: [number, number] = [500, 300];
 
-const propTypes = {
-  tableId: PropTypes.number.isRequired,
-  children: PropTypes.node,
-  placement: PropTypes.string,
-  offset: PropTypes.arrayOf(PropTypes.number),
+const mapStateToProps = (
+  state: any,
+  props: { tableId: number },
+): { table?: Table } => {
+  return {
+    table:
+      props.tableId == null
+        ? undefined
+        : Tables.selectors.getObject(state, {
+            entityId: props.tableId,
+          }),
+  };
 };
 
-type Props = { tableId: number } & Pick<
-  ITippyPopoverProps,
-  "children" | "placement" | "offset"
->;
+type Props = { tableId: number } & ReturnType<typeof mapStateToProps> &
+  Pick<ITippyPopoverProps, "children" | "placement" | "offset">;
 
 const className = "table-info-popover";
 
-function TableInfoPopover({ tableId, children, placement, offset }: Props) {
+function TableInfoPopover({
+  table,
+  tableId,
+  children,
+  placement,
+  offset,
+}: Props) {
   placement = placement || "left-start";
 
-  return tableId != null ? (
+  const hasDescription = !!table?.description;
+
+  return hasDescription ? (
     <TippyPopover
       className={className}
       interactive
@@ -52,10 +68,8 @@ function TableInfoPopover({ tableId, children, placement, offset }: Props) {
       {children}
     </TippyPopover>
   ) : (
-    children
+    <>{children}</>
   );
 }
 
-TableInfoPopover.propTypes = propTypes;
-
-export default TableInfoPopover;
+export default connect(mapStateToProps)(TableInfoPopover);
